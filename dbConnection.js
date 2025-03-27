1c151f0f489eaff7b94b4113049cb38d0b88a63a
Iber_Luis <132290429+iberluis@users.noreply.github.com>
2025-03-26 23:22:50 -0500

    Update dbConnection.js

--- dbConnection.js
+++ dbConnection.js
@@ -1,40 +1,15 @@
-const { Pool } = require('pg');  // Usamos Pool para manejar múltiples conexiones
+const { Pool } = require('pg');
 
-// Datos de conexión
 const pool = new Pool({
-  host: 'dpg-cveo6tbv2p9s73dosh5g-a.oregon-postgres.render.com',   // O la IP de tu servidor de base de datos
-  port: 5432,          // El puerto predeterminado de PostgreSQL
-  user: 'root',        // Reemplázalo con tu usuario de PostgreSQL
-  password: 'CVzQIR70nChKa9ovRb1Vom3NFo5GrvQ3',  // Reemplázalo con tu contraseña
-  database: 'campus_89ds',  // El nombre de la base de datos
-  ssl: {
-    rejectUnauthorized: false,  // Asegura que la conexión sea segura
-  }
+  host: 'dpg-cveo6tbv2p9s73dosh5g-a.oregon-postgres.render.com',
+  port: 5432,
+  user: 'root',
+  password: 'CVzQIR70nChKa9ovRb1Vom3NFo5GrvQ3',
+  database: 'campus_89ds',
+  ssl: { rejectUnauthorized: false },
 });
 
-// Función para ejecutar una consulta SQL
-async function ejecutarConsulta() {
-  try {
-    // Obtener una conexión del pool
-    const client = await pool.connect();
-    console.log("Conexión exitosa a la base de datos");
+// Exporta el pool para usarlo en otros archivos
+module.exports = pool;
 
-    // Ejecutar la consulta SQL
-    const result = await client.query('SELECT id, nombre_mesa, cantidad_asiento, estado FROM mesa');
-
-    console.log(result.rows);  // Mostrar los resultados
-    client.release();  // Liberar la conexión
-
-  } catch (error) {
-    console.error('Error de consulta:', error);
-  }
-}
-
-// Llamar a la función para ejecutar la consulta
-ejecutarConsulta();
-
-// Asegúrate de cerrar el pool cuando la aplicación termine
-process.on('exit', () => {
-  pool.end();
-});
 
